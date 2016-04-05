7941dc23995f7c92fd672651632dc13e8465fe1d
DeepwaterCreations <dylan.craine@deepwatercreations.com>
2016-04-04 23:19:52 -0600

    Whoops, accidentally left this line in

--- blog/blogCtrl.js
+++ blog/blogCtrl.js
@@ -1,4 +1,3 @@
-var deepwaterControllers = angular.module('deepwaterControllers', []);
 deepwaterControllers.controller('BlogCtrl', function($scope, $http){
     //Get the blog posts from the test json file. Later, from the PHP script that 
     //queries the database.
