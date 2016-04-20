1f353bb84b23cd3ecfe1ae38c69c24dbeba72ab6
DeepwaterCreations <dylan.craine@deepwatercreations.com>
2016-04-20 14:22:37 -0600

    Form for user comments

--- blog/blogPageCtrl.js
+++ blog/blogPageCtrl.js
@@ -1,6 +1,11 @@
 deepwaterControllers.controller('BlogPageCtrl', function($scope, $routeParams, $http){
     $scope.post = {};
     $scope.comments = [];
+    $scope.newcomment = {
+        title: "",
+        name: "",
+        text: ""
+    };
     $http.get('blogconnect.php', {params: {ID: $routeParams.ID, querytype: "blogposts"}}).
         //Get the post content
         then(function(data){
@@ -11,4 +16,11 @@ deepwaterControllers.controller('BlogPageCtrl', function($scope, $routeParams, $
         then($http.get('blogconnect.php', {params: {ID: $routeParams.ID, querytype: "blogcomments"}}).then(function(data){
             $scope.comments = data.data; 
         }));
+
+    $scope.postComment = function(){
+        $scope.newcomment.post = $scope.post.id;
+        $scope.newcomment.date = "Who knows?";
+        $scope.newcomment.id = "I'll let MySQL figure this one out.";
+        console.log($scope.newcomment);
+    };
 });
