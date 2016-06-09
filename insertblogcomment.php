c9d33087ce10d20c28dea108aaafe75b712ef750
DeepwaterCreations <dylan.craine@deepwatercreations.com>
2016-06-08 21:31:47 -0600

    Send email notification to me on comment
    I can't say for sure if this works until I try it live, since it relies
    on server-side email program configuration (I think?)

--- insertblogcomment.php
+++ insertblogcomment.php
@@ -5,15 +5,20 @@ $pdo = getConnection();
 $params = json_decode(file_get_contents('php://input'), true);
 
 if($params["comment"] != ""){
+    $postid = $params["postid"];
+    $title = $params["title"];
+    $name = ($params["name"] == "") ? 'Outis' : $params["name"];
+    $comment = $params["comment"];
+
     $post_comment_query = 'INSERT INTO blogcomments
         VALUES(NULL, :postid, CURDATE(), :title, :name, :comment)';
     $post_comment_prepared_statement = $pdo->prepare($post_comment_query);
 
     try{
-        $success = $post_comment_prepared_statement->execute(array(':postid'=>$params["postid"],
-                                                        ':title'=>$params["title"],
-                                                        ':name'=>($params["name"] == "") ? 'Outis' : $params["name"],
-                                                        ':comment'=>$params["comment"]));
+        $success = $post_comment_prepared_statement->execute(array(':postid'=>$postid,
+                                                        ':title'=>$title,
+                                                        ':name'=>($name == "") ? 'Outis' : $name,
+                                                        ':comment'=>$comment));
         $result = $post_comment_prepared_statement->fetchAll();
     } catch(Exception $fit){
         $pdo->rollBack();
@@ -22,7 +27,7 @@ if($params["comment"] != ""){
 
     if($success){
         /* Got to tell the blog post that it has a new comment */
-        $query = sprintf("UPDATE  blog  SET  comments  =  comments + 1 WHERE  id=" . $params["postid"]);    
+        $query = sprintf("UPDATE  blog  SET  comments  =  comments + 1 WHERE  id=" . $postid);    
         //TODO: protect the above query from SQL injection
         try{
             $pdo->beginTransaction();
@@ -34,9 +39,15 @@ if($params["comment"] != ""){
         }
 
         //This bit sends me an email notification.
-        /* $mailSubj = "Comment on post: " . $title; */
-        /* $mailBody = "http://deepwatercreations.com/blogpost.php?id=" . $postid . " " . $title . $name . $comment; */
-        /* mail($MyEmail, $mailSubj, $mailBody); */
+        $myEmail = "dylan.craine@deepwatercreations.com";
+        $mailSubj = "Comment on post: " . $title;
+        $mailBody = "http://deepwatercreations.com/blogpost.php?id=" . $postid . "\r\n" .
+            "TITLE: " . $title . "\r\n" .
+            "NAME: " . $name . "\r\n" . "\r\n" .
+            $comment;
+        //Word wrap because lines longer than 70 characters aren't allowed.
+        $mailBody = wordwrap($mailBody, 70, "\r\n");
+        mail($myEmail, $mailSubj, $mailBody, $headers);
     }
 }
 
