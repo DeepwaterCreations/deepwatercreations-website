1ba17bc9ecfb5b3da12b1d45014bfa7aedd8cd89
DeepwaterCreations <dylan.craine@deepwatercreations.com>
2017-11-13 19:18:41 -0700

    Update links/descriptions

--- sidebars/sidebarLeftCtrl.js
+++ sidebars/sidebarLeftCtrl.js
@@ -1,25 +1,30 @@
 deepwaterControllers.controller('SidebarLeftCtrl', function($scope, $http){
     $scope.linkslist = [
         {
-            title: "Dash Dev",
-            desc: "My friend Colin makes android apps and cool stuff.",
-            link: "https://dashdv.wordpress.com/"
+            title: "Oyster Squadron",
+            desc: "",
+            link: "http://connivance.net"
         },
         {
-            title: "Oyster Squadron",
-            desc: "I did not put this website in my list \
-                of sidebar links. The link showed up unexpectedly \
-                one day, complete with this very description \
-                of itself. I wouldn't click it if I were you. This \
-                site's pretty weird.",
-            link: "http://connivance.net/"
+            title: "Wanderer RPG Podcast",
+            desc: "",
+            link: "http://wanderer-rpg.com"
         },
         {
             title: "Partysheephats",
-            desc: "My friend Chris investigates quirky subjects in \
-                economics.",
+            desc: "",
             link: "https://partysheephats.com"
-        } 
+        },
+        {
+            title: "Dash Dev",
+            desc: "",
+            link: "https://dashdv.wordpress.com"
+        },
+        {
+            title: "Worlds Without Master",
+            desc: "",
+            link: "http://www.worldswithoutmaster.com"
+        }
     ];
     $scope.goToLink = function(link){
         window.location.href = link;
